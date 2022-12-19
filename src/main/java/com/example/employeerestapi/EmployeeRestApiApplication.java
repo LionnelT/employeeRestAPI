26fbb12d6f8b2e93a30e442b7c8981e629f7ea59
LionnelT <lionneltsuro56@gmail.com>
2022-12-19 15:47:50 +0200

    initial commit

---
+++ src/main/java/com/example/employeerestapi/EmployeeRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.employeerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestApiApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRestApiApplication.class, args);
+    }
+
+}
