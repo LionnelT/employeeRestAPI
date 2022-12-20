dfd2a7e19b890be3a77d09150d44366f6be1d614
LionnelT <lionneltsuro56@gmail.com>
2022-12-20 21:41:44 +0200

    added save employee controller

--- src/main/java/com/example/employeerestapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeerestapi/controller/EmployeeController.java
@@ -3,9 +3,7 @@ package com.example.employeerestapi.controller;
 import com.example.employeerestapi.model.Employee;
 import com.example.employeerestapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +17,13 @@ public class EmployeeController {
     //get all employees
     @GetMapping("employees")
     public List<Employee> getAllEmployees(){
+
         return employeeRepository.findAll();
+    }
+    //create employee
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 
 }
